78b8ff4d2ff18b428cd0a00dccb58c6dc4788569
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2025-04-01 21:52:40 -0600

    1ero
    hola

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace GenerarQr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.GenerarBtn = new System.Windows.Forms.Button();
+            this.GuardarBtn = new System.Windows.Forms.Button();
+            this.pbCodigoBarras = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ImportarBtn = new System.Windows.Forms.Button();
+            this.ExportarBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodigoBarras)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo de Barras";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(59, 85);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(190, 22);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // GenerarBtn
+            // 
+            this.GenerarBtn.Location = new System.Drawing.Point(298, 85);
+            this.GenerarBtn.Name = "GenerarBtn";
+            this.GenerarBtn.Size = new System.Drawing.Size(75, 23);
+            this.GenerarBtn.TabIndex = 2;
+            this.GenerarBtn.Text = "Generar";
+            this.GenerarBtn.UseVisualStyleBackColor = true;
+            this.GenerarBtn.Click += new System.EventHandler(this.GenerarBtn_Click);
+            // 
+            // GuardarBtn
+            // 
+            this.GuardarBtn.Location = new System.Drawing.Point(391, 84);
+            this.GuardarBtn.Name = "GuardarBtn";
+            this.GuardarBtn.Size = new System.Drawing.Size(75, 23);
+            this.GuardarBtn.TabIndex = 3;
+            this.GuardarBtn.Text = "Guardar";
+            this.GuardarBtn.UseVisualStyleBackColor = true;
+            this.GuardarBtn.Click += new System.EventHandler(this.GuardarBtn_Click);
+            // 
+            // pbCodigoBarras
+            // 
+            this.pbCodigoBarras.Location = new System.Drawing.Point(59, 125);
+            this.pbCodigoBarras.Name = "pbCodigoBarras";
+            this.pbCodigoBarras.Size = new System.Drawing.Size(432, 208);
+            this.pbCodigoBarras.TabIndex = 4;
+            this.pbCodigoBarras.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(543, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(555, 321);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ImportarBtn
+            // 
+            this.ImportarBtn.Location = new System.Drawing.Point(298, 24);
+            this.ImportarBtn.Name = "ImportarBtn";
+            this.ImportarBtn.Size = new System.Drawing.Size(75, 23);
+            this.ImportarBtn.TabIndex = 7;
+            this.ImportarBtn.Text = "Importar";
+            this.ImportarBtn.UseVisualStyleBackColor = true;
+            this.ImportarBtn.Click += new System.EventHandler(this.ImportarBtn_Click);
+            // 
+            // ExportarBtn
+            // 
+            this.ExportarBtn.Location = new System.Drawing.Point(391, 23);
+            this.ExportarBtn.Name = "ExportarBtn";
+            this.ExportarBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExportarBtn.TabIndex = 8;
+            this.ExportarBtn.Text = "Exportar";
+            this.ExportarBtn.UseVisualStyleBackColor = true;
+            this.ExportarBtn.Click += new System.EventHandler(this.ExportarBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 363);
+            this.Controls.Add(this.ExportarBtn);
+            this.Controls.Add(this.ImportarBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pbCodigoBarras);
+            this.Controls.Add(this.GuardarBtn);
+            this.Controls.Add(this.GenerarBtn);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodigoBarras)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button GenerarBtn;
+        private System.Windows.Forms.Button GuardarBtn;
+        private System.Windows.Forms.PictureBox pbCodigoBarras;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ImportarBtn;
+        private System.Windows.Forms.Button ExportarBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BarcodeLib;
+using System.Drawing.Imaging;
+
+namespace GenerarQr
+{
+    public partial class Form1 : Form
+    {
+        public static List<string> miLista = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerarBtn_Click(object sender, EventArgs e)
+        {
+            BarcodeLib.Barcode Codigo = new BarcodeLib.Barcode();
+            Codigo.IncludeLabel = true;
+            pbCodigoBarras.Image = Codigo.Encode(BarcodeLib.TYPE.CODE128, txtCodigo.Text, Color.Black, Color.White, 200, 100);
+        }
+            
+
+        private void GuardarBtn_Click(object sender, EventArgs e)
+        {
+            Image imgFinal = (Image)pbCodigoBarras.Image.Clone();
+
+            SaveFileDialog Caja = new SaveFileDialog();
+            Caja.AddExtension = true;
+            Caja.ShowDialog();
+            if(!string.IsNullOrEmpty(Caja.FileName))
+            {
+                imgFinal.Save(Caja.FileName, ImageFormat.Png);
+            }
+            imgFinal.Dispose();
+        }
+
+        private void ImportarBtn_Click(object sender, EventArgs e)
+        {
+
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Archivos de Excel|*.xlsx|Archivos de Excel 97-2003|*.xls" })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+
+                    {
+                        using (var stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read))
+
+                        {
+                            using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+
+                            {
+                                var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                                {
+                                    ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+
+
+                                    {
+                                        UseHeaderRow = true
+
+                                    }
+                                });
+
+                                dataGridView1.DataSource = dataSet.Tables[0];
+                            }
+
+                        }
+
+                        dataGridView1.Columns[0].HeaderText = "Equipo";
+                        dataGridView1.Columns[1].HeaderText = "Marca";
+                        dataGridView1.Columns[2].HeaderText = "Serie";
+
+                        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                        dataGridView1.AutoResizeColumns();
+                        dataGridView1.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11);
+
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        private void ExportarBtn_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow fila in dataGridView1.Rows)
+            {
+                string Codigo = fila.Cells[2].Value.ToString();
+
+                Zen.Barcode.Code128BarcodeDraw mGeneradorCB = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+                pbCodigoBarras.Image = mGeneradorCB.Draw(Codigo, 60);
+
+                var codigoBarras = pbCodigoBarras.Image;
+                var imagenCompleta = new Bitmap(codigoBarras.Width, codigoBarras.Height + 20);
+                var x = imagenCompleta.Width / 2;
+                var y = imagenCompleta.Height;
+
+                using (var grafic = Graphics.FromImage(imagenCompleta))
+                using (var sFormat = new StringFormat()
+                {
+                    Alignment = StringAlignment.Center,
+                    LineAlignment = StringAlignment.Far,
+                })
+                {
+                    grafic.Clear(Color.Transparent);
+                    grafic.DrawImage(codigoBarras, 2, 2);
+                    grafic.DrawString(Codigo, new Font("Arial", 10),
+                        Brushes.Black, x, y, sFormat);
+                }
+                pbCodigoBarras.Image = imagenCompleta;
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+                saveFileDialog.FileName = Codigo + ".png";
+
+                if (saveFileDialog.ShowDialog() != DialogResult.Cancel)
+                {
+                    string codigo_barras = saveFileDialog.FileName;
+                    Bitmap bitmap = new Bitmap(pbCodigoBarras.Image);
+                    bitmap.Save(codigo_barras);
+                }
+            }
+        }
+    }
+}
